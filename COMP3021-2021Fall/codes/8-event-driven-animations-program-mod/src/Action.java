ba0ee1fea3e695685107feafb92b201d5e8d37a9
enor2017 <enor2017@163.com>
2022-03-05 02:01:23 +0800

    Delete unnecessary courses.

--- COMP3021-2021Fall/codes/8-event-driven-animations-program-mod/src/Action.java
+++
@@ -1,5 +0,0 @@
-@FunctionalInterface
-public interface Action {
-  void run(String param);
-//  void stop(String param);
-}
--- COMP3021-2021Fall/codes/8-event-driven-animations-program-mod/src/TestInnerStatic.java
+++
@@ -1,8 +0,0 @@
-public class TestInnerStatic {
-    class Inner {
-        static int i;
-        static void f() {}
-        Inner() { ++i; }
-        static {}
-    }
-}
